5a98980fb7c2e6f5ab3df392912073656541babe
Misti Stevens <misti@mhsecure.com>
2015-11-25 16:16:13 -0500

    Initial Commit

---
+++ BrainControlledRobotTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BrainControlledRobotTest
+//
+//  Created by misti stevens on 11/25/15.
+//  Copyright (c) 2015 Misti Stevens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
