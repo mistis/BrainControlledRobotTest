4caf5f4f44cc81abbad99f8832a775856cceaeab
Misti Stevens <misti@mhsecure.com>
2015-11-30 22:15:50 -0500

    Create mock class.

--- BrainControlledRobotTest/CommandPacketGeneratorTest.cpp
+++ BrainControlledRobotTest/CommandPacketGeneratorTest.cpp
@@ -6,5 +6,20 @@
 //  Copyright (c) 2015 Misti Stevens. All rights reserved.
 //
 
-#include "NXTCommands.h"
+#include "gmock/gmock.h"
+#include "Commands.h"
 
+
+class CommandMock : public Commands {
+public:
+    
+    CommandMock() {}
+    
+    MOCK_METHOD0(moveLeft, void(void));
+    MOCK_METHOD0(moveRight, void(void));
+    MOCK_METHOD0(spin, void(void));
+    MOCK_METHOD0(moveForward, void(void));
+    MOCK_METHOD0(moveBackward, void(void));
+    MOCK_METHOD0(stopMoving, void(void));
+    MOCK_METHOD0(shoot, void(void));
+};
